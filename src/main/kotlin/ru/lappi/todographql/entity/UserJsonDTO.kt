0677048b4529fcbe94f475852226b24889cbea5d
Nikita Gorodilov <healyou1994@gmail.com>
2022-07-11 21:21:02 +0300

    Добавлен запуск на docker

--- src/main/kotlin/ru/lappi/todographql/entity/UserJsonDTO.kt
+++ src/main/kotlin/ru/lappi/todographql/entity/UserJsonDTO.kt
@@ -1,6 +1,10 @@
 package ru.lappi.todographql.entity
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 class UserJsonDTO {
+    @JsonProperty("userId")
     var user_id: Long? = null
+    @JsonProperty("privilegeCodes")
     var privilege_codes: List<String>? = null
 }
